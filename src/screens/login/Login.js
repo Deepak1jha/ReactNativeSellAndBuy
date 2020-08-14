53f1b22de4c887a1a4b977411815e47f00cd75be
Deepak jha <ddjha171@gmail.com>
2020-08-15 02:52:42 +0530

    Form Validation complete

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import {Image, StyleSheet} from "react-native";
+import {Button, Image, StyleSheet, Text} from "react-native";
 import Screen from "../../component/screen/Screen";
 import AppTextInput from "../../component/appTextInput/AppTextInput";
 import AppButton from "../../component/button/AppButton";
 import {Formik} from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  username: Yup.string().required().email().label("Username"),
+  password: Yup.string().required().min(4).label("Password")
+})
 
 export default function Login() {
 
   return (
     <Screen style={styles.container}>
       <Image source={require("../../../assets/logo-red.png")} style={styles.logo}/>
-
-      <Formik initialValues={{username: "", password: ""}} onSubmit={values => console.log(values)}>
-        {({handleChange, handleSubmit}) => (
+      <Formik initialValues={{username: "", password: ""}}
+              onSubmit={values => console.log(values)}
+              validationSchema={validationSchema}
+      >
+        {({handleChange, handleSubmit, errors}) => (
           <>
             <AppTextInput
               icon="email"
@@ -22,6 +30,7 @@ export default function Login() {
               keyboardType={"email-address"}
               onChangeText={handleChange("username")}
             />
+            <Text style={{color: "red"}}>{errors.username}</Text>
             <AppTextInput
               autoCorrect={false}
               autoCapitilize="none"
@@ -30,7 +39,9 @@ export default function Login() {
               secureTextEntry
               onChangeText={handleChange("password")}
             />
+            <Text style={{color: "red"}}>{errors.password}</Text>
             <AppButton title="Login" onPress={handleSubmit}/>
+            <Button title="click" onPress={handleSubmit}/>
           </>
         )}
       </Formik>
