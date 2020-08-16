aa493245cecbdf51bcbfaa41c7aa95f5ba3137d3
Deepak jha <ddjha171@gmail.com>
2020-08-17 00:19:18 +0530

    Stack Navigator

--- App.js
+++ App.js
@@ -1,15 +1,43 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import Screen from "./src/component/screen/Screen";
-import ListingEditScreen from "./src/screens/listingEditScreen/ListingEditScreen";
+import {StyleSheet, Text, View,Button} from 'react-native';
+import {createStackNavigator} from "@react-navigation/stack";
+import {NavigationContainer} from '@react-navigation/native';
+
+const Stack = createStackNavigator();
+
+
+function product({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Product List Screen</Text>
+      <Button title="Click" onPress={()=>navigation.navigate("Product_Detail",{name:"Deepak kumar jha"})}/>
+    </View>
+  );
+}
+
+function productDetail({route}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>{route.params.name}</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <Screen>
-      <ListingEditScreen/>
-    </Screen>
+    <NavigationContainer>
+      <StackNavigator/>
+    </NavigationContainer>
   );
 }
+
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Product" component={product}/>
+    <Stack.Screen name="Product_Detail" component={productDetail}/>
+  </Stack.Navigator>
+)
+
 
 const styles = StyleSheet.create({
   container: {
